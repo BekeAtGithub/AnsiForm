2e1978fae9e0ae4175b256a072aeb96ec2953a51
bekeatgithub <adammartonbeke@gmail.com>
2024-10-28 22:58:11 -0700

    terraform update2

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,11 +3,3 @@ provider "aws" {
   region = var.region
 }
 
-# Optional - Configure Backend for State Storage
-terraform {
-  backend "s3" {
-    bucket = var.state_bucket_name
-    key    = var.state_key
-    region = var.region
-  }
-}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,7 @@
 variable "region" {
   description = "AWS region for deploying resources"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "vpc_id" {
@@ -77,4 +78,33 @@ variable "state_bucket_name" {
 variable "state_key" {
   description = "Key for the Terraform state file in S3"
   type        = string
+}
+
+variable "private_subnets" {
+  description = "List of private subnet IDs for the EKS cluster"
+  type        = list(string)
+}
+
+variable "node_desired_size" {
+  description = "Desired number of nodes in the node group"
+  type        = number
+  default     = 2
+}
+
+variable "node_max_size" {
+  description = "Maximum number of nodes in the node group"
+  type        = number
+  default     = 5
+}
+
+variable "node_min_size" {
+  description = "Minimum number of nodes in the node group"
+  type        = number
+  default     = 1
+}
+
+variable "node_instance_type" {
+  description = "EC2 instance type for EKS nodes"
+  type        = string
+  default     = "t3.medium"
 }
